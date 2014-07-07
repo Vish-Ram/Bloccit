f0a5bd36e5b204455647988f7e60bea9ab0f58f7
vagrant <vagrant@rails-dev-box>
2014-07-07 23:08:02 +0000

    Associated and nested topics and posts.

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,10 +20,19 @@ users = User.all
 
 # The `save` method then saves this User to the database.
 
+15.times do
+   Topic.create(
+     name:         Faker::Lorem.sentence,
+     description:  Faker::Lorem.paragraph
+   )
+ end
+ topics = Topic.all
  
  # Create Posts
  50.times do
    Post.create(
+    user:   users.sample,
+     topic:  topics.sample,
      title:  Faker::Lorem.sentence,
      body:   Faker::Lorem.paragraph
    )
@@ -37,10 +46,34 @@ users = User.all
      body: Faker::Lorem.paragraph
    )
  end
- User.first.update_attributes(
-  email: 'youremail.com',
-  password: 'helloworld',
-)
+ # Create an admin user
+ admin = User.new(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+ admin.skip_confirmation!
+ admin.save
+ 
+ # Create a moderator
+ moderator = User.new(
+   name:     'Moderator User',
+   email:    'moderator@example.com', 
+   password: 'helloworld',
+   role:     'moderator'
+ )
+ moderator.skip_confirmation!
+ moderator.save
+ 
+ # Create a member
+ member = User.new(
+   name:     'Member User',
+   email:    'member@example.com',
+   password: 'helloworld',
+ )
+ member.skip_confirmation!
+ member.save
 
 puts "Seed finished"
 puts "#{User.count} users created"
