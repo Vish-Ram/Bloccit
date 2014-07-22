2ccaa2f71a1fd312f0a84cb5a790ef70ca2dac58
vagrant <vagrant@rails-dev-box>
2014-07-22 00:26:03 +0000

    privated topics

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,6 @@
 class TopicsController < ApplicationController
   def index
-    @topics = Topic.paginate(page: params[:page], per_page: 10) # add this line
+    @topics = Topic.visible_to(current_user).paginate(page: params[:page], per_page: 10) # add this line
     authorize @topics
   end
 
